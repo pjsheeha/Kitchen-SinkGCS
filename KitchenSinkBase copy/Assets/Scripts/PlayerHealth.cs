9d81947184552dea36d329e3331e3e7147777d73
Peter <pjsheeha@andrew.cmu.edu>
2018-09-19 21:53:05 -0400

    Kitchen Sink
    The first version of the multiplayer game

---
+++ KitchenSinkBase copy/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour
+{
+    [SerializeField] int maxHealth = 3;
+
+    [SyncVar(hook = "OnHealthChanged")] int health; //if we need the client to update their health they use this, 
+    //whenever server updates this, do function
+
+    Player player;
+
+
+    void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    [ServerCallback]
+    void OnEnable()
+    {
+        health = maxHealth;
+    }
+
+    [Server]
+    public bool TakeDamage()
+    {
+        bool died = false;
+
+        if (health <= 0)
+            return died;
+
+        health--;
+        died = health <= 0;
+
+        RpcTakeDamage(died);
+
+        return died;
+    }
+
+    [ClientRpc]
+    void RpcTakeDamage(bool died)
+    {
+        if (isLocalPlayer)
+            PlayerCanvas.canvas.FlashDamageEffect(); // causes ui element to flash
+
+        if (died)
+            player.Die();
+    }
+
+    void OnHealthChanged(int value) // a callback - define rules for when server sends new value
+    {
+        health = value;
+        if (isLocalPlayer)
+            PlayerCanvas.canvas.SetHealth(value); // when health changes, call this method
+    }
+}
---
+++ KitchenSinkBase copy/Assets/Scripts/ShotEffectsManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ShotEffectsManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] AudioSource gunAudio;
+    [SerializeField] GameObject impactPrefab;
+
+    ParticleSystem impactEffect;
+
+    //Create the impact effect for our shots
+    public void Initialize()
+    {
+        impactEffect = Instantiate(impactPrefab).GetComponent<ParticleSystem>();
+    }
+
+    //Play muzzle flash and audio
+    public void PlayShotEffects()
+    {
+        muzzleFlash.Stop(true);
+        muzzleFlash.Play(true);
+        gunAudio.Stop();
+        gunAudio.Play();
+    }
+
+    //Play impact effect and target position
+    public void PlayImpactEffect(Vector3 impactPosition)
+    {
+        impactEffect.transform.position = impactPosition;
+        impactEffect.Stop();
+        impactEffect.Play();
+    }
+}
